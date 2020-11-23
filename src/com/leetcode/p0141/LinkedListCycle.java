370e6a1bd49a120cb9cd83312e64a781d4530732
赵工 <4808844@qq.com>
2020-11-23 14:03:59 +0800

    从 leetcode 上复制回来代码有点失误。

--- src/com/leetcode/p0141/LinkedListCycle.java
+++ src/com/leetcode/p0141/LinkedListCycle.java
@@ -28,7 +28,7 @@ public class LinkedListCycle {
     public static boolean hasCycle(ListNode head) {
         if (head == null) return false;
         ListNode slow = head, fast = head;
-        while (slow.next != null && fast.next.next != null && fast.next.next != null) {
+        while (slow.next != null && fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) return true;
